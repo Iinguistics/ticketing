1df696d37fb3d5830e76ecbab7d66b5da4fd5f77
James <Thylinguistics@gmail.com>
2024-04-03 15:37:12 -0700

    adds base create route

---
+++ orders/src/routes/create.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { requireAuth, validateRequest } from '@jmsgoytia-ticketing/common';
+// import CreateController from '../Controllers/CreateController';
+import prefix from './prefix';
+
+const router = express.Router();
+
+router.post(
+	`${prefix}/orders`,
+	requireAuth,
+	[
+		body('ticket_id')
+			.notEmpty()
+			.withMessage('ticket id is required')
+			.isMongoId()
+			.withMessage('id must be a valid ObjectId'),
+	],
+	validateRequest,
+	async (req: Request, res: Response) => {
+		// return CreateController.handle(req, res);
+	}
+);
+
+export { router as createRouter };
---
+++ orders/src/routes/index.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import { createRouter } from './create';
+
+const routes: Router[] = [
+	createRouter,
+];
+
+export default routes;
