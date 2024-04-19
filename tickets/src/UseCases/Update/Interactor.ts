1a2aec4ce24799a31c22fd1df195b986de57fdff
James <Thylinguistics@gmail.com>
2024-04-18 17:01:48 -0700

    adds check for reserved ticket

--- tickets/src/UseCases/Update/Interactor.ts
+++ tickets/src/UseCases/Update/Interactor.ts
@@ -1,11 +1,13 @@
 import { natsWrapper } from '../../NatsWrapper';
 import {
+	BadRequestError,
 	Id,
 	NotAuthorizedError,
 	NotFoundError,
 } from '@jmsgoytia-ticketing/common';
 import Interactor from '../../UseCase/Interactor';
 import OkHttpPresenter from '../../Presenters/OkPresenter';
+import Ticket from '../../Entities/Ticket';
 import TicketUpdatedPublisher from '../../events/publishers/TicketUpdatedPublisher';
 import TicketRepository from '../../Repositories/TicketRepository';
 import UpdateRequest from './UpdateRequest';
@@ -19,18 +21,11 @@ class UpdateInteractor extends Interactor {
 	}
 
 	async _execute(req: UpdateRequest): Promise<UpdateResponse> {
-		const id = new Id(req.id);
 		const userId = new Id(req.userId);
 
-		const ticket = await this.#ticketRepository.getById(id);
+		const ticket = await this.#getTicketById(new Id(req.id));
 
-		if (!ticket) {
-			throw new NotFoundError();
-		}
-
-		if (!ticket.userId.equals(userId)) {
-			throw new NotAuthorizedError();
-		}
+		UpdateInteractor.#checkTicket(ticket, userId);
 
 		ticket.price = req.price;
 		ticket.title = req.title;
@@ -48,6 +43,28 @@ class UpdateInteractor extends Interactor {
 		return {
 			id: ticket.id.value,
 		};
+	}
+
+	async #getTicketById(id: Id): Promise<Ticket> {
+		const ticket = await this.#ticketRepository.getById(id);
+
+		if (!ticket) {
+			throw new NotFoundError();
+		}
+
+		return ticket;
+	}
+
+	static #checkTicket(ticket: Ticket, userId: Id): void {
+		if (!ticket.userId.equals(userId)) {
+			throw new NotAuthorizedError();
+		}
+
+		if (ticket.orderId) {
+			throw new BadRequestError(
+				`Cannot edit, ticket ID: ${ticket.id.value} is reserved`
+			);
+		}
 	}
 }
 
