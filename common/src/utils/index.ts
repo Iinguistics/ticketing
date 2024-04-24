7aebb61da32162cde3e0570a9c96220a4450e1fc
James <Thylinguistics@gmail.com>
2024-04-24 13:02:39 -0700

    adds state validation & updates common module

--- common/src/utils/index.ts
+++ common/src/utils/index.ts
@@ -1,5 +1,6 @@
 import camelToSnake from './camelToSnake';
 import checkEnvVars from './checkEnvVars';
 import createObjectId from './createObjectId';
+import isValidState from './isValidState';
 
-export { camelToSnake, checkEnvVars, createObjectId };
+export { camelToSnake, checkEnvVars, createObjectId, isValidState };
--- tickets/src/routes/create.ts
+++ tickets/src/routes/create.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
-import { requireAuth, validateRequest } from '@jmsgoytia-ticketing/common';
+import {
+	isValidState,
+	requireAuth,
+	validateRequest,
+} from '@jmsgoytia-ticketing/common';
 import CreateController from '../Controllers/CreateController';
 import prefix from './prefix';
 
@@ -10,7 +14,20 @@ router.post(
 	`${prefix}/tickets`,
 	requireAuth,
 	[
+		body('city').trim().notEmpty().withMessage('City required'),
+		body('description').trim().notEmpty().withMessage('Description required'),
+		body('postal_code').trim().notEmpty().withMessage('Postal code required'),
 		body('price').isFloat({ gt: 0 }).withMessage('Invalid Price'),
+		body('state')
+			.trim()
+			.notEmpty()
+			.withMessage('State required')
+			.custom(isValidState)
+			.withMessage('Invalid state'),
+		body('street_address')
+			.trim()
+			.notEmpty()
+			.withMessage('Street address required'),
 		body('title').trim().notEmpty().withMessage('Title required'),
 	],
 	validateRequest,
