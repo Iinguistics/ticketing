581a8a8d917a9b11536bfb503bad19f4a6175a5f
James <Thylinguistics@gmail.com>
2024-04-30 09:57:46 -0700

    adds aws config & sqs to listener

--- mailing/src/events/listeners/paymentCreatedListener.ts
+++ mailing/src/events/listeners/paymentCreatedListener.ts
@@ -4,7 +4,11 @@ import {
 	PaymentCreatedEvent,
 	Subjects,
 } from '@jmsgoytia-ticketing/common';
-import { QUEUE_GROUP_NAME } from '../../local/config';
+import { sqs } from '../../local/aws';
+import {
+	SEND_ORDER_NOTIFICATION_QUEUE_URL,
+	QUEUE_GROUP_NAME,
+} from '../../local/config';
 
 class PaymentCreatedListener extends Listener<PaymentCreatedEvent> {
 	readonly subject = Subjects.PaymentCreated;
@@ -13,7 +17,26 @@ class PaymentCreatedListener extends Listener<PaymentCreatedEvent> {
 	async onMessage(
 		data: PaymentCreatedEvent['data'],
 		msg: Message
-	): Promise<void> {}
+	): Promise<void> {
+		const params = {
+			MessageBody: JSON.stringify({
+				email: data.email,
+				paymentId: data.id,
+				orderId: data.orderId,
+			}),
+			QueueUrl: SEND_ORDER_NOTIFICATION_QUEUE_URL,
+		};
+
+		const response = await sqs.sendMessage(params).promise();
+
+		if (!response.MessageId) {
+			throw new Error(
+				`Could not send payment ID: ${data.id} to queue for order notification`
+			);
+		}
+
+		msg.ack();
+	}
 }
 
 export default PaymentCreatedListener;
--- mailing/src/local/config.ts
+++ mailing/src/local/config.ts
@@ -1,1 +1,3 @@
 export const QUEUE_GROUP_NAME = 'mailing-service';
+export const SEND_ORDER_NOTIFICATION_QUEUE_URL =
+	'https://sqs.us-east-2.amazonaws.com/441047657872/ticketing-send-order-notification';
